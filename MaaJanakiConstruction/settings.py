0208d1124dcca302d3723de721db18ad45c9b16a
Ashish Ranjan <ashish.ranjan.official.it@gmail.com>
2025-02-02 21:41:14 +0530

    Email added to django app

--- MaaJanakiConstruction/settings.py
+++ MaaJanakiConstruction/settings.py
@@ -131,3 +131,9 @@ MEDIA_URL='/media/'
 MESSAGE_TAGS = {
     messages.ERROR:'danger'
 }
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'maajanakiconstruction@gmail.com'
+EMAIL_HOST_PASSWORD = 'kavo rnla yajo pyod'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
--- design_app/views.py
+++ design_app/views.py
@@ -4,6 +4,9 @@ from .models import VedioGallary
 from .models import Contact
 from .models import FeedBack
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
+
 
 def design(request):
       con ={"intro":"WelCome To Maa Janaki Construction",
@@ -24,6 +27,19 @@ def contact(request):
           else:
             contact=Contact(name=name,mobile=mobile,email=email,msz=msz) 
             contact.save() 
+            form_data = {
+            'name':name,
+            'email':email,
+            'phone':mobile,
+            'message':msz,
+            }
+            message = '''
+            From:\t\t{}\n
+            Message:\t\t{}\n
+            Email:\t\t{}\n
+            Phone:\t\t{}\n
+            '''.format(form_data['name'], form_data['message'], form_data['email'],form_data['phone'])
+            send_mail(f'You got a Contact mail From -{name}', message, '', [settings.EMAIL_HOST_USER])
             messages.success(request, "Your message has been successfully sent")
     return render(request,'contact.html',con)
 
@@ -62,5 +78,26 @@ def feedback(request):
           else:
             feedback=FeedBack(name=name,mobile=mobile,email=email,feedback=msz,interior=interior,exterior=exterior,overall=overall,suggest=suggest) 
             feedback.save() 
+            form_data = {
+            'name':name,
+            'email':email,
+            'phone':mobile,
+            'message':msz,
+            'intrat':interior,
+            'extrat':exterior,
+            'overat':overall,
+            'suggest':suggest
+            }
+            message = '''
+            From:\t\t{}\n
+            Message:\t\t{}\n
+            Email:\t\t{}\n
+            Phone:\t\t{}\n
+            Interior_Rating:\t\t{}\n
+            Exterior_Rating:\t\t{}\n
+            Overall_Rating:\t\t{}\n
+            Suugest_to_other:\t\t{}\n
+            '''.format(form_data['name'], form_data['message'], form_data['email'],form_data['phone'],form_data['intrat'],form_data['extrat'],form_data['overat'],form_data['suggest'])
+            send_mail(f'FeedBack from -{name}', message, '', [settings.EMAIL_HOST_USER])
             messages.success(request, "Your Feedback has been successfully Submitted")
     return render(request,'feedback.html',con)
